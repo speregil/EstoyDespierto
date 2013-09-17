741769f2937b3d09727ae98fcb4bf32599b0fc57
Sebastian Gil Parga <s.gilparga@gmail.com>
2013-09-16 21:50:07 -0500

    Escena de la cocina

---
+++ Assets/Scripts/AdminCocina.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdminCocina : MonoBehaviour {
+
+	//Objetos
+	private GameObject Global;
+	private Parpado1 parpado1;
+	private Parpado2 parpado2;
+	private TextoDisplay textos;
+	private MovimientoDisplay movimiento;
+	private VariablesGlobales globales;
+	
+	//Flags particulares
+	
+	
+	//=================================================================================================
+	// inicializacion
+	//=================================================================================================
+	
+	void Awake(){
+		
+	}
+	
+	void Start () {
+		//Inicializa las relaciones con los scripts de control
+		Global = GameObject.Find("Global");
+		parpado1 = (Parpado1)GameObject.Find("Parpado1").GetComponent(typeof(Parpado1));
+		parpado2 = (Parpado2)GameObject.Find("Parpado2").GetComponent(typeof(Parpado2));
+		globales = (VariablesGlobales)Global.GetComponent(typeof(VariablesGlobales));
+		movimiento = (MovimientoDisplay)Global.GetComponent(typeof(MovimientoDisplay));
+		movimiento.EstablecerCamara(GameObject.Find("Main Camera"));
+		textos = (TextoDisplay)Global.GetComponent(typeof(TextoDisplay));
+		textos.cambiarAdmin();
+		//Cambia al grafo respectivo y se mueve al estado apropiado
+		movimiento.cambiarGrafo(EstadosNivel.PRINCIPAL);
+		movimiento.cambiarAdmin();
+		movimiento.reiniciarFlechas();
+		NodoGrafo ultimo = globales.darUltimoEstado();
+		movimiento.irAEstado(ultimo);
+		movimiento.reiniciarFlechas();
+		ultimo = movimiento.darEstadoActual();
+		if(ultimo.TieneAdelante())
+			movimiento.SiHayAdelante();
+		if(ultimo.TieneDetras())
+			movimiento.SiHayDetras();
+		if(ultimo.TieneIzquierda())
+			movimiento.SiHayIzquierda();
+		if(ultimo.TieneDerecha())
+			movimiento.SiHayDerecha();
+		
+		parpado1.Abrir();
+		parpado2.Abrir();
+	}
+	
+	// ============================================================================================
+	// Eventos
+	//=============================================================================================
+	
+	void Update () {
+	
+	}
+	
+	public void EventSwitch(string comando){
+		
+		
+		
+	}
+	
+	public void EventDialog(int resultado){
+		
+		
+	}
+	
+	public void EventEstado (string comando){
+		
+		
+	}
+	
+	//================================================================================================
+	// Corutinas auxiliares
+	//================================================================================================
+	
+	private IEnumerator Parpadear(){
+		parpado1.Cerrar();
+		parpado2.Cerrar();
+		yield return new WaitForSeconds(2);
+		parpado1.Abrir();
+		parpado2.Abrir();
+	}
+}
